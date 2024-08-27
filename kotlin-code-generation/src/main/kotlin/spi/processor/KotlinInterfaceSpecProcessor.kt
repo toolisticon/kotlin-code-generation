2a4f4dc70ffd55f249cd5e1fc077743def8996eb
Jan Galinski <jan.galinski@holisticon.de>
2024-08-27 08:32:13 +0200

    chore: missing processors
    see #20

---
+++ kotlin-code-generation/src/main/kotlin/spi/processor/KotlinInterfaceSpecProcessor.kt
@@ -0,0 +1,29 @@
+package io.toolisticon.kotlin.generation.spi.processor
+
+import com.squareup.kotlinpoet.ExperimentalKotlinPoetApi
+import io.toolisticon.kotlin.generation.builder.KotlinInterfaceSpecBuilder
+import io.toolisticon.kotlin.generation.spi.KotlinCodeGenerationContext
+import io.toolisticon.kotlin.generation.spi.KotlinCodeGenerationSpi
+import kotlin.reflect.KClass
+
+/**
+ * Used to implement a [io.toolisticon.kotlin.generation.spi.KotlinCodeGenerationProcessor]
+ * that will visit/modify a [KotlinInterfaceSpecBuilder].
+ */
+@ExperimentalKotlinPoetApi
+abstract class KotlinInterfaceSpecProcessor<CONTEXT : KotlinCodeGenerationContext<CONTEXT>, INPUT : Any>(
+  contextType: KClass<CONTEXT>,
+  inputType: KClass<INPUT>,
+  order: Int = KotlinCodeGenerationSpi.DEFAULT_ORDER
+) : KotlinCodeGenerationProcessorBase<CONTEXT, INPUT, KotlinInterfaceSpecBuilder>(
+  contextType = contextType,
+  inputType = inputType,
+  builderType = KotlinInterfaceSpecBuilder::class,
+  order = order
+) {
+  abstract override fun invoke(context: CONTEXT, input: INPUT?, builder: KotlinInterfaceSpecBuilder): KotlinInterfaceSpecBuilder
+  override fun test(context: CONTEXT, input: Any?): Boolean = super.test(context, input)
+}
+
+
+
---
+++ kotlin-code-generation/src/main/kotlin/spi/processor/KotlinTypeAliasSpecProcessor.kt
@@ -0,0 +1,29 @@
+package io.toolisticon.kotlin.generation.spi.processor
+
+import com.squareup.kotlinpoet.ExperimentalKotlinPoetApi
+import io.toolisticon.kotlin.generation.builder.KotlinTypeAliasSpecBuilder
+import io.toolisticon.kotlin.generation.spi.KotlinCodeGenerationContext
+import io.toolisticon.kotlin.generation.spi.KotlinCodeGenerationSpi
+import kotlin.reflect.KClass
+
+/**
+ * Used to implement a [io.toolisticon.kotlin.generation.spi.KotlinCodeGenerationProcessor]
+ * that will visit/modify a [KotlinTypeAliasSpecBuilder].
+ */
+@ExperimentalKotlinPoetApi
+abstract class KotlinTypeAliasSpecProcessor<CONTEXT : KotlinCodeGenerationContext<CONTEXT>, INPUT : Any>(
+  contextType: KClass<CONTEXT>,
+  inputType: KClass<INPUT>,
+  order: Int = KotlinCodeGenerationSpi.DEFAULT_ORDER
+) : KotlinCodeGenerationProcessorBase<CONTEXT, INPUT, KotlinTypeAliasSpecBuilder>(
+  contextType = contextType,
+  inputType = inputType,
+  builderType = KotlinTypeAliasSpecBuilder::class,
+  order = order
+) {
+  abstract override fun invoke(context: CONTEXT, input: INPUT?, builder: KotlinTypeAliasSpecBuilder): KotlinTypeAliasSpecBuilder
+  override fun test(context: CONTEXT, input: Any?): Boolean = super.test(context, input)
+}
+
+
+
