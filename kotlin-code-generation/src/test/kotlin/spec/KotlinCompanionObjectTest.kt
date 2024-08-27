6fc9659068ddccb0eb6a63c097d109cf98a4e5e9
Jan Galinski <jan.galinski@holisticon.de>
2024-08-27 16:51:25 +0200

    chore: more kdoc

--- kotlin-code-generation/src/test/kotlin/spec/KotlinCompanionObjectTest.kt
+++ kotlin-code-generation/src/test/kotlin/spec/KotlinCompanionObjectTest.kt
@@ -4,22 +4,17 @@ package io.toolisticon.kotlin.generation.spec
 
 import com.squareup.kotlinpoet.ExperimentalKotlinPoetApi
 import com.squareup.kotlinpoet.KModifier
-import io.toolisticon.kotlin.generation.KotlinCodeGeneration
+import io.toolisticon.kotlin.generation.KotlinCodeGeneration.buildClass
 import io.toolisticon.kotlin.generation.KotlinCodeGeneration.buildCompanionObject
 import io.toolisticon.kotlin.generation.KotlinCodeGeneration.format.FORMAT_STRING
-import io.toolisticon.kotlin.generation.TestFixtures.notDeprecated
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assumptions
-import org.junit.jupiter.api.Assumptions.assumeFalse
-import org.junit.jupiter.api.Assumptions.assumeTrue
 import org.junit.jupiter.api.Test
 
 internal class KotlinCompanionObjectTest {
 
   @Test
   fun build() {
-    val r = KotlinCodeGeneration.buildClass("foo", "Dummy") {
+    val r = buildClass("foo", "Dummy") {
       addType(buildCompanionObject() {
         addProperty("name", String::class) {
           addModifiers(KModifier.CONST)
@@ -28,12 +23,14 @@ internal class KotlinCompanionObjectTest {
       })
     }
 
-    assertThat(r.code).isEqualToIgnoringWhitespace("""
+    assertThat(r.code).isEqualToIgnoringWhitespace(
+      """
       public class Dummy {
         public companion object {
            public const val name: kotlin.String = "hello world"
         }
       }
-    """.trimIndent())
+    """.trimIndent()
+    )
   }
 }
